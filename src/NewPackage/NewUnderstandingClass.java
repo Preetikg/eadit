5bdb3572a199fb2cc97f6873dcc1dcd753dab487
abhis <abhis@Abhishekpc.fios-router.home>
2023-01-29 23:40:03 -0500

    local commit

---
+++ src/NewPackage/NewUnderstandingClass.java
@@ -0,0 +1,10 @@
+package NewPackage;
+
+public class NewUnderstandingClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is one test");
+	}
+
+}
