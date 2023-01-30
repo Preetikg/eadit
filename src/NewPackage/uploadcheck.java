8ee733888815ac2b225e609a2e4a5e0e8a804633
Preetikg <122491138+Preetikg@users.noreply.github.com>
2023-01-30 00:14:14 -0500

    uploadcheck

---
+++ src/NewPackage/uploadcheck.java
@@ -0,0 +1,11 @@
+package NewPackage;
+
+public class uploadcheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is uploadcheck");
+
+	}
+
+}
