160fb6cb57e28ab5f47f40b384a177486572c418
abhis <abhis@Abhishekpc.fios-router.home>
2023-01-29 23:42:49 -0500

    local second commit

--- src/NewPackage/NewUnderstandingClass.java
+++ src/NewPackage/NewUnderstandingClass.java
@@ -5,6 +5,9 @@ public class NewUnderstandingClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("This is one test");
+
+System.out.println("This is second test");
+
 	}
 
 }
